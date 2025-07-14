fdd4a5bd4f6c06468403f06249c37eb0737da3c6
RiyaRani14 <riyarani1404@gmail.com>
2025-07-14 20:01:49 +0530

    Check Odd or Even Number

--- Mathamatics/EvenOdd.java
+++ Mathamatics/EvenOdd.java
@@ -5,9 +5,8 @@ public class EvenOdd {
         if(n%2==0){
            return(true);
         }
-        else{
-            return(false);
-        }        
+        else
+            return(false);       
     }
     public static void main (String args[]){
         Scanner sc =new Scanner(System.in);
