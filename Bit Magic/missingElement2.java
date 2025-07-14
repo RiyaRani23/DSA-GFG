fd576ac0d8f14a25226753f16ca9d4241f2cb54a
RiyaRani14 <riyarani1404@gmail.com>
2025-07-14 19:59:40 +0530

    Missing element in an array using XOR

--- Bit Magic/missingElement2.java
+++ Bit Magic/missingElement2.java
@@ -1,4 +1,17 @@
 
 public class missingElement2 {
+    int missingNum(int arr[]) {
+        // code here
+        int XORall=0;
+        int XOR_array=0;
+        int n=arr.length+1;
+        for(int i=1;i<=n;i++){
+             XORall ^=i;
+        }
+        for(int i=0;i<arr.length;i++){
+            XOR_array ^=arr[i];
+        }
+        return XORall^ XOR_array;
+    }
     
 }
