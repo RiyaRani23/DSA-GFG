a06857c5e464fa56c81f58d5f170d83e8521e1bd
RiyaRani14 <riyarani1404@gmail.com>
2025-07-14 13:28:52 +0530

    Sum Of Even and Odd Digits

---
+++ Mathamatics/SumEvenDigits.java
@@ -0,0 +1,23 @@
+package Mathamatics;
+
+public class SumEvenDigits {
+    public static void main(String args []){
+        int n=68;
+        int sumEven=0;
+        int sumOdd=0;
+        while(n>0){
+            int lastDigit=n%10;
+            if (lastDigit%2==0){
+                 sumEven+=lastDigit;
+            }
+            else{
+                sumOdd+=lastDigit;
+            }
+            n=n/10;
+        }
+        System.out.println("sum of even digits is "+ sumEven + " sum of odd digits is " + sumOdd);
+
+
+    }
+    
+}
