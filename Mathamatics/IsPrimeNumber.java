0e87c5d1e9927d223a2e5f40be37cb5712bfdc3a
RiyaRani14 <riyarani1404@gmail.com>
2025-07-14 20:06:31 +0530

    Prime number or not

--- Mathamatics/IsPrimeNumber.java
+++ Mathamatics/IsPrimeNumber.java
@@ -2,7 +2,6 @@ package Mathamatics;
 
 public class IsPrimeNumber {
     static boolean isPrime(int n) {
-        // code here
        if(n==1) return false;
        if(n==2|| n==3) return true;
        if(n%2==0 || n%3==0) 
