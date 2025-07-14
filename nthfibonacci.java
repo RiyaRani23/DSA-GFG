7eda064c16e747b6d81326b89e20eb520247b68a
RiyaRani14 <riyarani1404@gmail.com>
2025-07-14 13:05:34 +0530

    N th Fibonacci Number

---
+++ nthfibonacci.java
@@ -0,0 +1,9 @@
+public class nthfibonacci {
+     public int nthFibonacci(int n) {
+        // code here
+        if (n==1 || n==0){
+           return n;
+        }
+        return (nthFibonacci(n-1)+ nthFibonacci(n-2));
+    }
+}
