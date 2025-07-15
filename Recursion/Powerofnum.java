6c7e4a76716dbc051df3acc72ac391bd25093d50
RiyaRani14 <riyarani1404@gmail.com>
2025-07-15 19:33:52 +0530

    Power Using Recursion

--- Recursion/Powerofnum.java
+++ Recursion/Powerofnum.java
@@ -1,7 +1,10 @@
 package Recursion;
 
 public class Powerofnum {
-    if(p==0) return 1;
+     static int RecursivePower(int n, int p) {
+        // Code here
+        if(p==0) return 1;
         int power = n* RecursivePower(n,p-1);
         return power;
+    }
 }
