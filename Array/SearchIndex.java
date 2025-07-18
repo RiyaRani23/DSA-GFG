e63eda293777d627639ff50292bd9da67260343d
RiyaRani14 <riyarani1404@gmail.com>
2025-07-18 21:40:25 +0530

    Search index in array

---
+++ Array/SearchIndex.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class SearchIndex {
+    public static int searchIndex(int arr[],int key,int i){
+        if (i==arr.length){
+            return 0;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+      
+        return searchIndex(arr,key,i+1);
+
+    }
+}
