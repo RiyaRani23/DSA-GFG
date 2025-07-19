3b7d096c918d8a9a35ca6f6bf898b42d6226bff5
RiyaRani14 <riyarani1404@gmail.com>
2025-07-19 07:13:38 +0530

    Indexes of Subarray Sum

---
+++ Array/SubArraySum.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.ArrayList;
+
+public class SubArraySum {
+     static ArrayList<Integer> subarraySum(int[] arr, int target) {
+       ArrayList <Integer> index = new ArrayList<>();
+            int left=0, sum=0;
+            for (int right=0 ; right<arr.length ; right++ ){
+                sum += arr[right];
+                while(sum > target && left<= right ){
+                    sum -=arr[left];
+                    left++;
+                }
+                if(sum==target){
+                    // 1 based indexing
+                     index.add(left +1 );
+                     index.add(right +1);
+                     return index;
+                }
+                
+        }
+        index.add(-1);
+        return index;
+    }
+}
+
