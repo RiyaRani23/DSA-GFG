39c48067c66ca7a90cb2a05c328e62bb25e1325b
RiyaRani14 <riyarani1404@gmail.com>
2025-07-26 23:35:23 +0530

    Majority Element in an array

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -1,18 +1,41 @@
 public class MajorityElement {
     int majorityElement(int arr[]) {
-    // brute force method
-        int n=arr.length;
-        int majority=n/2;
-        for (int i=0; i<n;i++){
-            int count = 0;
-            for(int j=0; j< n;j++){
-                if (arr[i]==arr[j]) count++;
-                if(majority < count)
-                   return arr[i];
+    // BRUTE FORCE METHOD
+        // int n=arr.length;
+        // int majority=n/2;
+        // for (int i=0; i<n;i++){
+        //     int count = 0;
+        //     for(int j=0; j< n;j++){
+        //         if (arr[i]==arr[j]) count++;
+        //         if(majority < count)
+        //            return arr[i];
+        //     }
+        // }
+        // return -1;
+        int count = 0,candidate = 0;
+        //  Find a candidate
+        for (int i = 0; i < arr.length; i++) {
+            if (count == 0) {
+                candidate = arr[i];
+                count = 1;
+            } else {
+                if (arr[i] == candidate)  count++;
+                else     count--;
             }
         }
-        return -1;
-        
-    }
 
+        // Step 2: Confirm the candidate
+        count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == candidate) {
+                count++;
+            }
+        }
+
+        if (count > arr.length / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
 }
