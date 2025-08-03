133f10def04ea0bdccfe67810179aaf3c478e54b
RiyaRani14 <riya14042012@gmail.com>
2025-08-03 17:39:20 +0530

    Floor in a Sorted Array

---
+++ Mathamatics/CountDigits.java
@@ -0,0 +1,17 @@
+package Mathamatics;
+
+public class CountDigits {
+     static int evenlyDivides(int n) {
+        int lastDigit = 0 ;
+        int num = n;
+        int count = 0;
+        while(n > 0){
+            lastDigit= n % 10;
+            if (lastDigit != 0 && num % lastDigit == 0){
+                count ++;
+            }
+            n = n/10;
+        }
+        return count;
+    }
+}
