af450098ca629d0a81e37ce054bd213a2655ea59
RiyaRani14 <riya14042012@gmail.com>
2025-08-16 14:24:00 +0530

    Allocate Minimum Pages

---
+++ Array/MinimumPages.java
@@ -0,0 +1,48 @@
+public class MinimumPages {
+    public int findPages(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        // no. of books less than student
+        if (k > n) return -1;
+
+        int minPages = 0, sum = 0 ;  
+        
+        for (int i = 0; i < n; i++){
+             minPages = Math.max(minPages, arr[i]);
+            sum += arr[i];
+        } 
+        
+        int maxPages = sum ;
+        int ans = -1;
+        int mid = (maxPages + minPages)/2 ;
+        while(minPages <= maxPages){
+            
+            if (isPossible(arr, n, k, mid)){
+                ans = mid;
+                maxPages = mid-1;
+            }
+            else {
+                minPages = mid + 1;
+            }
+            mid = (maxPages + minPages) / 2;
+        }
+        
+        return ans;
+    }
+    public boolean isPossible (int[] arr, int n , int k , int mid){
+        int studentCount = 1;
+        int pageSum = 0;
+        
+        for (int i=0 ; i<n ; i++){
+            if(pageSum + arr[i] <= mid){
+                pageSum += arr[i];
+            }
+            else {
+                studentCount ++;
+                if (studentCount > k || arr[i] > mid)  return false;
+                pageSum = arr[i];
+            }
+        }
+        return true;
+    }
+}
