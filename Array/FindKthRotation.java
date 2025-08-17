e7f670276d7021870dd78843b7907d1c992ec165
RiyaRani23 <riyarani1404@gmail.com>
2025-08-17 21:39:20 +0530

    Find Kth Rotation

---
+++ Array/FindKthRotation.java
@@ -0,0 +1,31 @@
+public class FindKthRotation {
+     public int findKRotation(int arr[]) {
+        // Code here
+        int n = arr.length;
+       int start = 0 , end = n-1;
+       while(start <= end){
+        //   if subarray already sorted
+           if(arr[start] < arr[end])  return start;
+           int mid = (start + end) / 2; 
+            int next = (mid + 1) % n;
+            int prev = (mid + n - 1) % n;
+            
+            // Check if mid is minimum
+            if (arr[mid] <= arr[next] && arr[mid] <= arr[prev]) {
+                return mid;
+            }
+            
+            // Decide which half to move
+            if (arr[mid] >= arr[start]) {
+                // Left half is sorted - go right
+                start = mid + 1;
+            } else {
+                // Right half is sorted - go left
+                end = mid - 1;
+            }
+        }
+     
+          return 0;
+    }
+}
+
