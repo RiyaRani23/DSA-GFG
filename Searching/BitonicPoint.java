1511b5aef06955374a1ce2a797f931943e5bb85a
RiyaRani23 <riyarani1404@gmail.com>
2025-08-25 11:16:48 +0530

    Bitonic Point

---
+++ Searching/BitonicPoint.java
@@ -0,0 +1,20 @@
+public class BitonicPoint {
+    public int findMaximum(int[] arr) {
+        // code here
+        int n = arr.length;
+        int low = 0 , high = n-1;
+        while(low  <= high){
+            int mid = (low + high )/ 2;
+            
+            if(arr[mid] > arr[mid - 1]){
+                low = mid + 1;
+            }
+           
+            else   {
+                high = mid - 1;
+            }
+        }
+        return arr[low - 1];
+    }
+}
+
