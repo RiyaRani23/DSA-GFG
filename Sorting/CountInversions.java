817dacf10783983bd8592399a7c29c45ed518370
RiyaRani23 <riyarani1404@gmail.com>
2025-08-27 13:34:13 +0530

    Count Inversions

--- Sorting/CountInversions.java
+++ Sorting/CountInversions.java
@@ -1,47 +1,48 @@
 package Sorting;
 
 public class CountInversions {
-    static int inversionCount(int arr[]) {
-        return mergeSortAndCount(arr, 0, arr.length - 1);
+   static int inversionCount(int arr[]) {
+        // Code Here
+        return mergeSort(arr,0,arr.length-1);
     }
-
-    static int mergeSortAndCount(int[] arr, int left, int right) {
-        int count = 0;
-        if (left < right) {
-            int mid = (left + right) / 2;
-
-            count += mergeSortAndCount(arr, left, mid);
-            count += mergeSortAndCount(arr, mid + 1, right);
-            count += mergeAndCount(arr, left, mid, right);
+    static int mergeSort(int[] arr, int left , int right){
+        int res = 0 ; 
+       
+        if (left < right){
+            int mid = left + (right - left)/2;
+            // count inversion in left half
+            res += mergeSort(arr , left , mid);
+            // count inversion in right half
+            res += mergeSort(arr , mid + 1, right);
+            // count 
+            res += countAndMerge(arr,left,mid,right);
+         }
+         return res;
+    }
+    // Merge two sorted array and count inversion
+    static int  countAndMerge(int[] arr ,int left , int mid , int right){
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for(int i=0 ; i < n1; i++) {
+            L[i] =arr[ left + i];
         }
-        return count;
-    }
-
-    static int mergeAndCount(int[] arr, int left, int mid, int right) {
-        int[] leftArr = new int[mid - left + 1];
-        int[] rightArr = new int[right - mid];
-
-        for (int i = 0; i < leftArr.length; i++)
-            leftArr[i] = arr[left + i];
-        for (int i = 0; i < rightArr.length; i++)
-            rightArr[i] = arr[mid + 1 + i];
-
-        int i = 0, j = 0, k = left, swaps = 0;
-
-        while (i < leftArr.length && j < rightArr.length) {
-            if (leftArr[i] <= rightArr[j]) {
-                arr[k++] = leftArr[i++];
+         for(int i=0 ; i < n2; i++) {
+            R[i] = arr[mid + 1 + i];
+        }
+        int i = 0 , j = 0 , k = left ,swap = 0;
+        while(i < n1 && j < n2){
+            if(L[i] <= R[j]){
+                arr[k++] = L[i++];
             } else {
-                arr[k++] = rightArr[j++];
-                swaps += (leftArr.length - i);  // All remaining elements in leftArr are greater
+                arr[k++] = R[j++];
+                swap += (n1 - i);
             }
         }
+         while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
 
-        while (i < leftArr.length)
-            arr[k++] = leftArr[i++];
-        while (j < rightArr.length)
-            arr[k++] = rightArr[j++];
-
-        return swaps;
+        return swap;
     }
 }
